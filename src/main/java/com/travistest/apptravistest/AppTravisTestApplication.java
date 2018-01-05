54b3b9d2a20d4b192e528e52e3a438ea431f63f2
Anh <aionee79@icloud.com>
2018-01-05 09:57:52 +0000

    first commit

---
+++ src/main/java/com/travistest/apptravistest/AppTravisTestApplication.java
@@ -0,0 +1,12 @@
+package com.travistest.apptravistest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTravisTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTravisTestApplication.class, args);
+	}
+}
